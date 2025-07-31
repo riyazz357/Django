7ee263b89fc9be811f8d5a30a0e6d95d50c63046
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-07-31 22:39:12 +0530

    add customized class in admin.py

--- jadid/admin.py
+++ jadid/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import chaiVariety
+from .models import chaiVariety,chaiCertificate,chaiReview,Store
 
 # Register your models here.
-admin.site.register(chaiVariety)
+class ChaiReviewInLine(admin.TabularInline):
+    model=chaiReview
+    extra=2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display=('name','type','date_added',)
+    inlines=[ChaiReviewInLine]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    filter_horizontal=('chai_varities',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display=('chai','certificate_number')
+
+
+admin.site.register(chaiVariety,ChaiVarietyAdmin)
+admin.site.register(Store,storeAdmin)
+admin.site.register(chaiCertificate,ChaiCertificateAdmin)
+
